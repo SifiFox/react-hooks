78f2f9df3465c8c4eab67a5321058408c657cb99
SifiFox <dima.antonovich.1999@gmail.com>
2023-04-17 14:08:11 +0300

    feat: add useCalendar; feat: add useToggle;

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
+import useToggle from "./hooks/useToggle";
 function App() {
+  const [isVisible, toggleVisible] = useToggle(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isVisible && <div>test</div>}
+
+      <button onClick={toggleVisible}>Change text visible</button>
     </div>
   );
 }
